49bc941d84a8cee1b3f08a00790088206f24ca4f
Rawdrey <rawdrey@gmail.com>
2024-11-17 09:49:28 -0300

    add pagina afiliados e estyles.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,6 @@
-// Aguarda o carregamento completo do DOM antes de executar o script
 document.addEventListener("DOMContentLoaded", () => {
-    const body = document.querySelector("body");
-
-    // Função para carregar o footer
-    fetch("footer.html")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Erro ao carregar o footer: " + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            // Insere o conteúdo do footer no final do body
-            body.insertAdjacentHTML("beforeend", data);
-        })
-        .catch(error => console.error("Erro:", error));
-});
+    // Atualizar automaticamente o ano no rodapé
+    const anoAtual = new Date().getFullYear();
+    document.getElementById("ano-atual").textContent = anoAtual;
+  });
+  
